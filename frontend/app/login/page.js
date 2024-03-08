72e2439893c1853df1d24c08684975d8ddcc89f9
na0260 <samjr.008@gmail.com>
2024-03-08 22:49:22 +0600

    login page added

---
+++ frontend/app/login/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LoginCard from "@/app/components/LoginCard";
+import Navbar from "@/app/components/Navbar";
+
+const Page = () => {
+    return (
+        <>
+            <div className="container mx-auto lg:px-96 lg:py-60">
+                <LoginCard/>
+            </div>
+        </>
+    );
+};
+
+export default Page;
