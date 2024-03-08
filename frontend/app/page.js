15ea931be46eb898cdf153e7afd65856b9bf9b38
na0260 <samjr.008@gmail.com>
2024-03-08 22:49:40 +0600

    modified

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,5 +1,6 @@
 import Navbar from "@/app/components/Navbar";
 import ProductList from "@/app/components/ProductList";
+import LoginCard from "@/app/components/LoginCard";
 
 export default function Home() {
   return (
