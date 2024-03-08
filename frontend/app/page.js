bc7801f50839eb6628c63a87e9dae6d9cbd338ff
na0260 <samjr.008@gmail.com>
2024-03-08 22:29:53 +0600

    products added to home pages

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,10 +1,13 @@
 import Navbar from "@/app/components/Navbar";
+import ProductList from "@/app/components/ProductList";
 
 export default function Home() {
   return (
     <>
         <div className="container mx-auto">
-        <Navbar/>
+            <Navbar/>
+
+            <ProductList/>
         </div>
     </>
   );
