995022f9551260f48681913323cc1f5f39dabe37
na0260 <samjr.008@gmail.com>
2024-03-08 22:29:10 +0600

    product card component added

---
+++ frontend/app/components/ProductCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const ProductCard = (props) => {
+    return (
+        <>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure><img src={props.image} alt={props.title}/>
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                        {props.title}
+                        <div className="badge badge-secondary">{props.price} BDT</div>
+                    </h2>
+                    <p>{props.description}</p>
+                    <div className="card-actions justify-end">
+                        <div className="badge badge-outline">{props.qty} pcs</div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default ProductCard;
